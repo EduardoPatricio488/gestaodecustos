c6bb76f56c95b1955b2f8fcc9f1339adff123050
Eduardo Patrício <eduardopatricio166@gmail.com>
2026-09-11 13:11:28 +0100

    Ensure offline bunker services load before the UI

--- resources/js/offline-bunker-boot.js
+++ resources/js/offline-bunker-boot.js
@@ -1,7 +1,8 @@
+import './offline-bunker';
+import './offline-bunker-ui';
+
 window.beginOfflineSession = function beginOfflineSession() {
     if (!localStorage.getItem('finance-pro-offline-started-at')) {
         localStorage.setItem('finance-pro-offline-started-at', String(Date.now()));
     }
 };
-
-import './offline-bunker-ui';
